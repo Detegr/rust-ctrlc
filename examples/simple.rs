483ce083626bd2cdb3be82c7e01a02bb61ccdaab
Antti Keränen <detegr@gmail.com>
2015-07-16 12:23:59 +0300

    Added warning to simple.rs

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,3 +1,8 @@
+// Warning: If you run this example with `cargo run --example simple`
+// and hit Ctrl-C, the executable will still be kept running using
+// 100% of CPU. This is because cargo seems to kill the process even
+// though you have set up Ctrl-C handler of your own.
+
 extern crate ctrlc;
 use ctrlc::CtrlC;
 fn main() {
