86412ec2bfd8124047ea964e7b22c7db66ac4809
Antti Keränen <detegr@gmail.com>
2016-02-10 18:46:38 +0200

    Add a way to specify the polling rate. Improve documentation

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,29 @@
 // notice may not be copied, modified, or distributed except
 // according to those terms.
 
-//! A simple easy to use wrapper around Ctrl-C signal.
+//! A simple easy to use wrapper around Ctrl-C.
+//! # Example
+//! ```no_run
+//! extern crate ctrlc;
+//! use ctrlc::CtrlC;
+//! use std::sync::atomic::{AtomicBool, Ordering};
+//! use std::sync::Arc;
+//!
+//! fn main() {
+//!     let running = Arc::new(AtomicBool::new(true));
+//!     let r = running.clone();
+//!     CtrlC::set_handler(move || {
+//!         r.store(false, Ordering::SeqCst);
+//!     });
+//!     println!("Waiting for Ctrl-C...");
+//!     while running.load(Ordering::SeqCst) {}
+//!     println!("Got it! Exiting...");
+//! }
+//! ```
 
 use std::sync::atomic::Ordering;
+use std::thread;
+use std::time;
 
 mod features {
     use std::sync::atomic::{AtomicBool, ATOMIC_BOOL_INIT};
@@ -56,22 +76,46 @@ use self::platform::*;
 
 pub struct CtrlC;
 impl CtrlC {
-    /// Sets up the signal handler for Ctrl-C
+    /// Sets up the signal handler for Ctrl-C using default polling rate of 100ms.
     /// # Example
     /// ```
-    /// use ctrlc::CtrlC;
+    /// # use ctrlc::CtrlC;
     /// CtrlC::set_handler(|| println!("Hello world!"));
     /// ```
-    pub fn set_handler<F: Fn() -> () + 'static + Send>(user_handler: F) -> () {
+    pub fn set_handler<F>(user_handler: F)
+        where F: Fn() -> () + 'static + Send
+    {
+        CtrlC::set_handler_with_polling_rate(user_handler, time::Duration::from_millis(100));
+    }
+
+    /// Sets up the signal handler for Ctrl-C using a custom polling rate.
+    /// The polling rate is the amount of time the internal spinloop of CtrlC sleeps between
+    /// iterations. Because condition variables are not safe to use inside a signal handler,
+    /// CtrlC (from version 1.1.0) uses a spinloop and an atomic boolean instead.
+    ///
+    /// Normally you should use `set_handler` instead, but if the default rate of  100 milliseconds
+    /// is too fast or too slow for you, you can use this routine instead to set your own.
+    /// # Example
+    /// ```
+    /// # use std::time::Duration;
+    /// # use ctrlc::CtrlC;
+    /// CtrlC::set_handler_with_polling_rate(
+    ///     || println!("Hello world!"),
+    ///     Duration::from_millis(10)
+    /// );
+    /// ```
+    pub fn set_handler_with_polling_rate<F>(user_handler: F, polling_rate: time::Duration)
+        where F: Fn() -> () + 'static + Send
+    {
         unsafe {
             set_os_handler(handler);
         }
-        ::std::thread::spawn(move || {
+        thread::spawn(move || {
             loop {
                 if DONE.compare_and_swap(true, false, Ordering::Relaxed) {
                     user_handler();
                 }
-                ::std::thread::sleep(std::time::Duration::from_millis(100));
+                thread::sleep(polling_rate);
             }
         });
     }
