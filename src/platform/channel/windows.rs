748e70c9a6ee39d9fd796c3b268e1b5457a8f6d4
Antti Keränen <detegr@gmail.com>
2018-04-19 22:17:05 +0300

    Start windows channel

--- src/platform/channel/windows.rs
+++ src/platform/channel/windows.rs
@@ -1,1 +1,16 @@
+use SignalType;
+use error::Error;
 
+pub type ChannelType = WindowsChannel;
+
+pub struct WindowsChannel;
+
+impl WindowsChannel {
+    pub fn new(signal: SignalType) -> Result<WindowsChannel, Error> {
+        Ok(WindowsChannel {})
+    }
+
+    pub fn recv(&self) -> Result<SignalType, Error> {
+        Ok(SignalType::Ctrlc)
+    }
+}
