20edfdc35ca3da00566ea5e6698e2585f8ebdecc
Hannu Hartikainen <hannu.hartikainen@gmail.com>
2019-04-15 13:05:37 +0300

    Reformat code to pass rustfmt

--- examples/readme_example.rs
+++ examples/readme_example.rs
@@ -18,7 +18,8 @@ fn main() {
     let r = running.clone();
     ctrlc::set_handler(move || {
         r.store(false, Ordering::SeqCst);
-    }).expect("Error setting Ctrl-C handler");
+    })
+    .expect("Error setting Ctrl-C handler");
     println!("Waiting for Ctrl-C...");
     while running.load(Ordering::SeqCst) {
         thread::sleep(time::Duration::from_millis(10));
