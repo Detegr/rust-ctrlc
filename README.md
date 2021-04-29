7838b91dcf8f99492530ea9844aa3ee04195ea3d
jikstra <jikstra@disroot.org>
2021-04-29 21:45:14 +0200

    cargo fmt

--- README.md
+++ README.md
@@ -14,9 +14,8 @@ use ctrlc;
 fn main() {
     let (tx, rx) = channel();
     
-    ctrlc::set_handler(move || {
-        tx.send(()).expect("Could not send signal on channel.")
-    }).expect("Error setting Ctrl-C handler");
+    ctrlc::set_handler(move || tx.send(()).expect("Could not send signal on channel."))
+        .expect("Error setting Ctrl-C handler");
     
     println!("Waiting for Ctrl-C...");
     rx.recv().expect("Could not receive from channel.");
--- examples/readme_example.rs
+++ examples/readme_example.rs
@@ -7,17 +7,16 @@
 // notice may not be copied, modified, or distributed except
 // according to those terms.
 
-use std::sync::mpsc::channel;
 use ctrlc;
+use std::sync::mpsc::channel;
 
 fn main() {
     let (tx, rx) = channel();
-    
-    ctrlc::set_handler(move || {
-        tx.send(()).expect("Could not send signal on channel.")
-    }).expect("Error setting Ctrl-C handler");
-    
+
+    ctrlc::set_handler(move || tx.send(()).expect("Could not send signal on channel."))
+        .expect("Error setting Ctrl-C handler");
+
     println!("Waiting for Ctrl-C...");
     rx.recv().expect("Could not receive from channel.");
-    println!("Got it! Exiting..."); 
+    println!("Got it! Exiting...");
 }
